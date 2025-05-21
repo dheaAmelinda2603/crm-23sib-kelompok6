75df7d8dfd4fa26673084c082fe1ff837a1f2201
allyfah tika <allyfah23si@mahasiswa.pcr.ac.id>
2025-05-21 15:39:55 +0700

    hi ketua, ni dari princess utk pelanggan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import CustomerManagement from "./pages/CustomerManagement";
 
 function App(){
   return(
     <Routes>
       <Route element={<MainLayout />}>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/pelanggan" element={<CustomerManagement />} />
+
       </Route>
     </Routes>
   )
